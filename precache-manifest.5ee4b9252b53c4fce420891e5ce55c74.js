52301b0251b739698830f3244365f43a86998875
Eric <jiangdaheda2@sina.com>
2020-11-16 13:25:10 +0800

    deploy

---
+++ precache-manifest.5ee4b9252b53c4fce420891e5ce55c74.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa97cccc9e050138aaa7",
+    "url": "/myresume-website/css/app.05fdfb97.css"
+  },
+  {
+    "revision": "1259595860a42d11835b",
+    "url": "/myresume-website/css/chunk-vendors.2322a959.css"
+  },
+  {
+    "revision": "4fe121772ce7688f2ddc84568f2510c9",
+    "url": "/myresume-website/img/1.4fe12177.png"
+  },
+  {
+    "revision": "7547c6f7fe4966555a6de1e7a3b154ef",
+    "url": "/myresume-website/img/2.7547c6f7.png"
+  },
+  {
+    "revision": "a8cc783b0756df6bd0bb676ff346ff7c",
+    "url": "/myresume-website/img/3.a8cc783b.png"
+  },
+  {
+    "revision": "a6fd5b6131a43bca94e278ad915b975b",
+    "url": "/myresume-website/img/4.a6fd5b61.png"
+  },
+  {
+    "revision": "45a9c3ac2c15142bdc9d07bbba099fd4",
+    "url": "/myresume-website/img/5.45a9c3ac.png"
+  },
+  {
+    "revision": "85cab87b05d6fc384b974e772d7f2ec5",
+    "url": "/myresume-website/img/6.85cab87b.png"
+  },
+  {
+    "revision": "8d974e9363ce2c1ea882a6a704efefbd",
+    "url": "/myresume-website/img/7.8d974e93.png"
+  },
+  {
+    "revision": "5e97c4df1b4798af9b953b1a52408b97",
+    "url": "/myresume-website/img/8.5e97c4df.png"
+  },
+  {
+    "revision": "d5ac23bda871961fd0c3706407b6d16a",
+    "url": "/myresume-website/img/9.d5ac23bd.png"
+  },
+  {
+    "revision": "37dfa0cd4c6fe791627185493a9a08ac",
+    "url": "/myresume-website/img/gh_f67f42cbc665_344的副本.37dfa0cd.jpg"
+  },
+  {
+    "revision": "994111b5442e8efbfa0f9a2370492b04",
+    "url": "/myresume-website/img/pikaqiu.994111b5.png"
+  },
+  {
+    "revision": "a23fb80c6876307ed650f71171368712",
+    "url": "/myresume-website/img/react.a23fb80c.png"
+  },
+  {
+    "revision": "ea88668c7e413945452fba61d939031b",
+    "url": "/myresume-website/img/vue.ea88668c.png"
+  },
+  {
+    "revision": "af8def65102c6830f3d73512ae593f87",
+    "url": "/myresume-website/img/背景的副本.af8def65.png"
+  },
+  {
+    "revision": "af8def65102c6830f3d73512ae593f87",
+    "url": "/myresume-website/img/背景的副本.png"
+  },
+  {
+    "revision": "ffb80414a694f793c881af9eb9b2fab6",
+    "url": "/myresume-website/index.html"
+  },
+  {
+    "revision": "aa97cccc9e050138aaa7",
+    "url": "/myresume-website/js/app.061ac4a1.js"
+  },
+  {
+    "revision": "1259595860a42d11835b",
+    "url": "/myresume-website/js/chunk-vendors.bbfc52e7.js"
+  },
+  {
+    "revision": "150066ccbc7aba08c770b90116b10089",
+    "url": "/myresume-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/myresume-website/robots.txt"
+  }
+]);
